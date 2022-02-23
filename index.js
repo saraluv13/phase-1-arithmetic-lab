098fa0fbe8728b710b61bc185086e9445f655750
Supaasara Bowman <supaasarabowman@Supaasaras-MacBook-Pro.local>
2022-02-23 16:47:21 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 
 // Write your code here
+let num1=31
+let num2=2
+let multiply = num1 * num2;
+
+let random = Math.round(Math.random()* 10) +1
+
+let num3 = 6
+let num4 = 16
+let mod= num4 % num3;
+
+let max = Math.max(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20);
+
+
+ 
